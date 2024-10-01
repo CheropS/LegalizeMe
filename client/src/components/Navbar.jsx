e48c943e2b282b1ad2e3d8c42f5540c819c91728
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-10-01 15:37:01 +0300

    Added company name

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,15 +35,15 @@ export default function Navbar() {
   return (
     <header className="bg-white">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+        <div className="flex lg:flex-1 items-center">
+          <a href="/" className="-m-1.5 p-1.5 flex items-center">
             <span className="sr-only">LegalizeMe</span>
             <img
               alt="Your Company Logo"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               className="h-8 w-auto"
             />
-            <h3 className="text-md font-bold text-gray-900">LegalizeMe</h3>
+            <h3 className="ml-3 text-md font-bold text-gray-900">LegalizeMe</h3>
           </a>
         </div>
 
@@ -120,14 +120,14 @@ export default function Navbar() {
         <div className="fixed inset-0 z-10" />
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5 flex items-center">
               <span className="sr-only">Your Company</span>
               <img
                 alt="Your Company Logo"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 className="h-8 w-auto"
               />
-              <h3 className="text-md font-bold text-gray-900">LegalizeMe</h3>
+              <h3 className="ml-3 text-md font-bold text-gray-900">LegalizeMe</h3>
             </a>
             <button
               type="button"
