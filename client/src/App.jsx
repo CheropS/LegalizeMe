e107f8ff167cea7151c88846c8f0344c743c8801
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-10-01 15:29:15 +0300

    Added navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Navbar from './components/Navbar';
+// import Signup from './components/Signup';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         {/*<Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />*/}
+        <Route path="/about" element={<About />} />
+        <Route path='/signup' element={<Signup />} />*/}
       </Routes>
     </Router>
   );
