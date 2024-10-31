6b9fa110e4b27b07ecf6149f7809766df575fd46
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-10-31 14:39:13 +0300

    Added the solutions page

--- client/src/components/Solutions.jsx
+++ client/src/components/Solutions.jsx
@@ -10,7 +10,9 @@ const Solutions = () => {
                 <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="max-w-2xl mx-auto text-center">
                         <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">How do we create success</h2>
-                        <p className="max-w-lg mx-auto mt-4 text-base leading-relaxed text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
+                        <p className="max-w-lg mx-auto mt-4 text-base leading-relaxed text-gray-600">
+                            We are committed to providing you with the best solutions for your legal needs and business growth. We approach every client with a focus on integrity, advocacy, and understanding. We fight for you!
+                        </p>
                     </div>
 
                     <ul className="max-w-md mx-auto mt-16 space-y-12">
@@ -24,7 +26,9 @@ const Solutions = () => {
                             </div>
                             <div className="ml-6">
                                 <h3 className="text-lg font-semibold text-black">Create a free account</h3>
-                                <p className="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                                <p className="mt-4 text-base text-gray-600">
+                                    Signup to LegalizeMe and get started with your legal needs. We provide you with the best solutions for your legal needs and business growth.
+                                </p>
                             </div>
                         </li>
 
@@ -37,8 +41,10 @@ const Solutions = () => {
                                 </svg>
                             </div>
                             <div className="ml-6">
-                                <h3 className="text-lg font-semibold text-black">Build your website</h3>
-                                <p className="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                                <h3 className="text-lg font-semibold text-black">Find Solutions to Your Legal Need</h3>
+                                <p className="mt-4 text-base text-gray-600">
+                                    Navigate to the cases section and find the best solutions to your legal needs. Get started with your LegalizeMe and explore the best solutions.
+                                </p>
                             </div>
                         </li>
 
@@ -49,18 +55,97 @@ const Solutions = () => {
                                 </svg>
                             </div>
                             <div className="ml-6">
-                                <h3 className="text-lg font-semibold text-black">Release & launch</h3>
-                                <p className="mt-4 text-base text-gray-600">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                                <h3 className="text-lg font-semibold text-black">Interact with "Counsel"</h3>
+                                <p className="mt-4 text-base text-gray-600">
+                                    Interact with "Counsel", our AI powered chatbot, to get instant solutions to your legal needs. Counsel is available 24/7 to help you with your queries. Generate legal documents and get them verified by our legal experts.
+                                </p>
                             </div>
                         </li>
                     </ul>
                 </div>
             </section>
 
+            <section class="py-10 bg-white sm:py-16 lg:py-24 animate-fadeIn font-roboto">
+                <div className="-mx-4 flex flex-wrap">
+                    <div className="w-full px-4">
+                        <div className="mx-auto mb-12 max-w-[510px] text-center lg:mb-20">
+                            <span className="mb-2 block text-lg font-semibold text-indigo-600 animate-fadeIn">
+                                Solutions
+                            </span>
+                            <h2 className="mb-3 text-3xl font-bold leading-[1.2] text-dark dark:text-black sm:text-4xl md:text-[40px] animate-fadeIn">
+                                Explore the wide range of solutions we offer to help you with your legal needs
+                            </h2>
+                            <p className="text-base text-body-color dark:text-dark-6 animate-fadeIn">
+                                We are committed to providing you with the best solutions for your legal needs and business growth. We approach every client with a focus on integrity, advocacy, and understanding. We fight for you!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div class="grid grid-cols-1 gap-12 text-center sm:grid-cols-2 md:grid-cols-3 lg:gap-y-16">
+                        <div>
+                            <div class="relative flex items-center justify-center mx-auto">
+                                <svg class="text-blue-100" width="72" height="75" viewBox="0 0 72 75" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M63.6911 28.8569C68.0911 48.8121 74.6037 61.2674 53.2349 65.9792C31.8661 70.6909 11.6224 61.2632 7.22232 41.308C2.82229 21.3528 3.6607 12.3967 25.0295 7.68503C46.3982 2.97331 59.2911 8.90171 63.6911 28.8569Z" />
+                                </svg>
+                                <svg class="absolute text-blue-600 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="1.5"
+                                        d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+                                    />
+                                </svg>
+                            </div>
+                            <h3 class="mt-8 text-lg font-semibold text-black">Comprehensive Legal Database</h3>
+                            <p class="mt-4 text-base text-gray-600">
+                                Imagine having the entire legal world at your disposal. Our platform gives you instant access to an extensive legal database packed with case law
+                                precedent and other legal documents. Say goodbye to endless searches - our AI-powered search engine delivers only the most relevant results, summarized and
+                                ready to use. Legal researc has never been this easy. Welcome to the future of law.
+                            </p>
+                        </div>
+
+                        <div>
+                            <div class="relative flex items-center justify-center mx-auto">
+                                <svg class="text-orange-100" width="62" height="64" viewBox="0 0 62 64" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M62 13.001C62 33.4355 53.9345 64.001 33.5 64.001C13.0655 64.001 0 50.435 0 30.0005C0 9.56596 2.56546 4.00021 23 4.00021C43.4345 4.00021 62 -7.43358 62 13.001Z" />
+                                </svg>
+                                <svg class="absolute text-orange-600 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                                </svg>
+                            </div>
+                            <h3 class="mt-8 text-lg font-semibold text-black">Automated Legal Document Generation</h3>
+                            <p class="mt-4 text-base text-gray-600">
+                                Why waste time on tedious paperwork? With just a few inputs, our system generates tailoremade lega documents in minutes - no hassle,
+                                no fuss. Whether it's contracts, agreements, or policies, we've got you covered. And for transparency, each document is watermarked
+                                as a draft until it is legally validated. Precision, speed, and control - right at your fingertips.
+                            </p>
+                        </div>
+
+                        <div>
+                            <div class="relative flex items-center justify-center mx-auto">
+                                <svg class="text-green-100" width="66" height="68" viewBox="0 0 66 68" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M65.5 30C65.5 50.4345 46.4345 68 26 68C5.56546 68 0 50.4345 0 30C0 9.56546 12.5655 0 33 0C53.4345 0 65.5 9.56546 65.5 30Z" />
+                                </svg>
+                                <svg class="absolute text-green-600 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                                </svg>
+                            </div>
+                            <h3 class="mt-8 text-lg font-semibold text-black">Your Legal Ally on Every Step of Your Journey</h3>
+                            <p class="mt-4 text-base text-gray-600">
+                                We are more than just a platform - we are your trusted legal ally. Whether you're drafting documents, conducting research, or navigating complexities, we're here to
+                                simplify the process, reduce uncertainity, and empower you to succeed. Our team of legal experts is always on standby to provide guidance and support. With LegalizeMe, you're never alone.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
             <section className="py-10 bg-gray-100 sm:py-16 lg:py-24 animate-fadeIn font-roboto">
                 <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="max-w-2xl mx-auto text-center">
-                        <h2 className="text-3xl font-bold leading-tight text-gray-800 sm:text-4xl lg:text-5xl">Trusted by <span className="text-blue-600">30k+</span> world className companies & design teams</h2>
+                        <h2 className="text-3xl font-bold leading-tight text-gray-800 sm:text-4xl lg:text-5xl">Trusted by <span className="text-blue-600">renowned</span> world className companies & design teams</h2>
                     </div>
 
                     <div className="grid max-w-xl grid-cols-1 mx-auto mt-8 text-center lg:max-w-full sm:mt-12 lg:mt-20 lg:grid-cols-3 gap-x-6 xl:gap-x-12 gap-y-6">
