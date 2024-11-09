5ccc22eabf066213a3ea7dfc12b3e8989d0ba1f7
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-11-09 12:53:11 +0300

    Updated resources page

--- client/src/components/Resources.jsx
+++ client/src/components/Resources.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import Navbar from './Navbar';
-import Breadcrumb from './Breadcrumb';
 import Constitution from './Constitution';
-import Footer from './Footer';
 
 const Resources = () => {
     return (
         <>
-            <Navbar />
             <Constitution />
-            <Footer />
         </>
     );
 }
